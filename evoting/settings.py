4d5dca7df17bac4d7867e31b174ad3834f623a73
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2020-01-27 15:58:03 +0200

    add detabase scription

--- evoting/settings.py
+++ evoting/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'accounts'
 ]
 AUTH_USER_MODEL = 'accounts.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -80,8 +81,11 @@ WSGI_APPLICATION = 'evoting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 }
 
